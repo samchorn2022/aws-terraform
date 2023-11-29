dfef32719a8e2b46c53455b8dcf3db3f8b03c8a0
LeavY <147359468+chhengy@users.noreply.github.com>
2023-11-29 08:58:07 +0700

    Create vars.tf

---
+++ vars.tf
@@ -0,0 +1,132 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  
+  tags = {
+    Name = "my-app-vpc"
+  }
+}
+
+resource "aws_subnet" "public_a" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "public_a"
+  }
+}
+
+resource "aws_subnet" "public_b" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "public_b"
+  }
+}
+
+resource "aws_subnet" "public_c" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.3.0/24"
+  availability_zone = "us-east-1c"
+
+  tags = {
+    Name = "public_c"
+  }
+}
+
+resource "aws_internet_gateway" "main-igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main-igw"
+  }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main-igw.id
+  }
+
+  tags = {
+    Name = "public_rt"
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "b" {
+  subnet_id      = aws_subnet.public_b.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "c" {
+  subnet_id      = aws_subnet.public_c.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_security_group" "my_sg" {
+  name        = "my-sg"
+  vpc_id      = aws_vpc.main.id
+
+
+  ingress {
+    description      = "Allow ALL from everywhere"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description      = "Allow outgoing traffic"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "my-sg"
+  }
+}
+
+resource "aws_lb_target_group" "my_tg" {
+  name     = "my-tg"
+  target_type = "instance"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+}
+
+resource "aws_launch_template" "my_launch_template" {
+
+  name = "my_launch_template"
+  
+  image_id = "ami-02ec85a32741e2526"
+  instance_type = "t2.micro"
+  key_name = "mykey"
+  
+  user_data = filebase64("${path.module}/server.sh")
+
+  block_device_mappings {
+    device_name = "/dev/sda1"
+
+    ebs {
+      volume_size = 8
+      volume_type = "gp2"
+    }
+  }
+
+  network_interfaces {
+    associate_public_ip_address = true
+    security_groups = [aws_security_group.my_sg.id]
+  }
+}
