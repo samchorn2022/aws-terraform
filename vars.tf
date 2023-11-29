f706058d5c5bd64a8eeb7b74c4e2068e9bf7ec7e
LeavY <147359468+chhengy@users.noreply.github.com>
2023-11-29 19:00:12 +0700

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -76,9 +76,8 @@ resource "aws_security_group" "my_sg" {
   name        = "my-sg"
   vpc_id      = aws_vpc.main.id
 
-
   ingress {
-    description      = "Allow ALL from everywhere"
+    description      = "Allow traffic from everywhere"
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
@@ -95,6 +94,25 @@ resource "aws_security_group" "my_sg" {
 
   tags = {
     Name = "my-sg"
+  }
+}
+
+resource "aws_lb" "my_alb" {
+  name               = "my-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.my_sg.id]
+  subnets            = [aws_subnet.public_a.id, aws_subnet.public_b.id, aws_subnet.public_c.id]
+}
+
+resource "aws_lb_listener" "my_lb_listener" {
+  load_balancer_arn = aws_lb.my_alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.my_tg.arn
   }
 }
 
@@ -129,4 +147,72 @@ resource "aws_launch_template" "my_launch_template" {
     associate_public_ip_address = true
     security_groups = [aws_security_group.my_sg.id]
   }
+}
+
+resource "aws_autoscaling_group" "my_asg" {
+  name                      = "my_asg"
+  max_size                  = 5
+  min_size                  = 2
+  health_check_type         = "ELB"
+  desired_capacity          = 2
+  target_group_arns = [aws_lb_target_group.my_tg.arn]
+
+  vpc_zone_identifier       = [aws_subnet.public_a.id, aws_subnet.public_b.id, aws_subnet.public_c.id]
+  
+  launch_template {
+    id      = aws_launch_template.my_launch_template.id
+    version = "$Latest"
+  }
+}
+
+resource "aws_autoscaling_policy" "scale_up" {
+  name                   = "scale_up"
+  policy_type            = "SimpleScaling"
+  autoscaling_group_name = aws_autoscaling_group.my_asg.name
+  adjustment_type        = "ChangeInCapacity"
+  scaling_adjustment     = "1"    # add one instance
+  cooldown               = "300"  # cooldown period after scaling
+}
+
+resource "aws_cloudwatch_metric_alarm" "scale_up_alarm" {
+  alarm_name          = "scale-up-alarm"
+  alarm_description   = "asg-scale-up-cpu-alarm"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "120"
+  statistic           = "Average"
+  threshold           = "50"
+  dimensions = {
+    "AutoScalingGroupName" = aws_autoscaling_group.my_asg.name
+  }
+  actions_enabled = true
+  alarm_actions   = [aws_autoscaling_policy.scale_up.arn]
+}
+
+resource "aws_autoscaling_policy" "scale_down" {
+  name                   = "asg-scale-down"
+  autoscaling_group_name = aws_autoscaling_group.my_asg.name
+  adjustment_type        = "ChangeInCapacity"
+  scaling_adjustment     = "-1"
+  cooldown               = "300"
+  policy_type            = "SimpleScaling"
+}
+
+resource "aws_cloudwatch_metric_alarm" "scale_down_alarm" {
+  alarm_name          = "asg-scale-down-alarm"
+  alarm_description   = "asg-scale-down-cpu-alarm"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "120"
+  statistic           = "Average"
+  threshold           = "30"
+  dimensions = {
+    "AutoScalingGroupName" = aws_autoscaling_group.my_asg.name
+  }
+  actions_enabled = true
+  alarm_actions   = [aws_autoscaling_policy.scale_down.arn]
 }
