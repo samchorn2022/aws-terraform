5705f91d9a13e24e12ba4037b4ec28ebf845f9df
LeavY <147359468+chhengy@users.noreply.github.com>
2023-11-29 08:56:17 +0700

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.49.0"
+    }
+  }
+}
+
+
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIAWU43ATGIZGPBOQX6"
+  secret_key = "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"
+}
